85190711cd0160c48e09513e27ed4e765a5cf943
Tuan Truong <truongvanminhtuan@gmail.com>
2017-09-25 16:06:30 +0700

    delay

--- index.js
+++ index.js
@@ -1,62 +1,66 @@
 var express     = require("express")
 var app         = express()
-var orm         = require("orm");
-var bodyParser  = require('body-parser')
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
+var orm         = require("orm")
+var say         = require('say')
 app.listen(8080)
-
-// orm.connect("mongodb://localhost/test", function (err, db) {
-//     if (err) throw err;})
-// orm.connectAsync('mongodb://localhost/test')
-//     .then((db) => {
-
-//     })
-//     .catch((error) => console.error('Connection error: '+error ))
- 
-app.use(orm.express('mongodb://localhost/student', {
+app.use(orm.express('mongodb://localhost/mytestdata', {
     define: function (db, models, next) {
-        models.person = db.define("student", { 
-            frirtName:String,
-            lastName:String,
-            class:String
-        },{
-            hooks: {
-                beforeCreate: function () {
-                return new Promise(function(resolve, reject) {
-                  if (this.class == "12.3") {
-                    return reject(new Error("No Does allowed class 12.3"));
-                  }
-                    return resolve();
-                  });
-                }
-              }
-        });
-        next();
+        models.message = db.define("messages", { 
+            message:String,
+            createdAt:String,
+            read: Boolean    
+        })
+        next()
     }
-}));
-app.get("/", function (req, res) {
-    req.models.person.findAsync().then(function(results) {
-        //res.send('welcome, ' + JSON.stringify(results))
-        console.log(JSON.stringify(results) )   
-      });    
-    res.end()
-});
-app.post("/",urlencodedParser,function(req,res) {
-    isnull(req.body.frirtName)
-    isnull(req.body.lastName)
-    isnull(req.body.class)
-    var newRecord = {};
-    newRecord.frirtName = req.body.frirtName
-    newRecord.lastName = req.body.lastName
-    newRecord.class = req.body.class
-    req.models.person.createAsync(newRecord)
-    .then(function(results) {
-      console.log(JSON.stringify(results))
-    });
-    res.end()
+}))
+app.get('/', function(req,res) {
+    if (req.query.createdAt ==null || req.query.createdAt== ''|| req.query.message == null || req.query.message == '') {
+        res.status(404)
+        res.end()
+    } else {       
+        var newRecord = {};
+        newRecord.message = req.query.message
+        newRecord.createdAt = req.query.createdAt
+        newRecord.read = false   
+        req.models.message.createAsync(newRecord)
+        .then(function(results) {
+            console.log(JSON.stringify(results))
+        });
+        //say.speak(req.query.message)
+        // setTimeout(function() {
+        //     say  
+            
+        //  }, 5000)     
+        // req.models.message.find({ read:"false" }, function (err, message) {
+        //      console.log("nno!"+JSON.stringify(req.models.message))
+        // })
+        req.models.message.findAsync({ }).then(function(results) {
+            //res.send('welcome, ' + JSON.stringify(results))
+            for(var i=0;i<results.length;i++){
+                    if(!results[i].read){
+                        var temp =results[i].message
+                setTimeout(function() {
+                say.speak(temp)
+            
+          }, 5000) 
+                   
+                    results[i].read=true
+                    results[i].save(function(err){
+                        console.log("saved!");
+                    })
+                    console.log(results[i].read ) 
+            }
 
+            }
+            //console.log(results[0].message )   
+          });    
+
+        res.status(200)
+        res.end()
+    }
 })
-//////////////////////////////////////////////////////////////////
-function isnull(string){
-    if(string == null ||string == '') return res.sendStatus(400)
+function sleep(ms){
+    return new Promise(resolve=>{
+        setTimeout(resolve,ms)
+    })
 }
